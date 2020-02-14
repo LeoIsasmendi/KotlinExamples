2f3b2e7c266eb2283eb48de4050e9b7898631488
Leonardo Isasmendi <leo.isasmendi@gmail.com>
2020-02-13 23:20:42 -0300

    fixed search items layout

--- ImdbExample/app/src/main/java/com/example/imdbexample/Fragments/FavoritesFragment.kt
+++ ImdbExample/app/src/main/java/com/example/imdbexample/Fragments/FavoritesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.imdbexample.LocalStorage.LocalStorage
@@ -16,6 +17,7 @@ import kotlinx.android.synthetic.main.fragment_favorites.*
 
 class FavoritesFragment : Fragment() {
 
+    private var columnCount = 2
     private lateinit var mAdapter: GenericRecyclerViewAdapter<MovieDetailsResponse>
 
     override fun onCreateView(
@@ -44,7 +46,10 @@ class FavoritesFragment : Fragment() {
 
         }
 
-        list.layoutManager = LinearLayoutManager(context)
+        list.layoutManager = when {
+            columnCount <= 1 -> LinearLayoutManager(context)
+            else -> GridLayoutManager(context, columnCount)
+        }
         list.adapter = mAdapter
     }
 
