4a17d995c77941305dfd6afecaf7ad006af35c8c
Leonardo Isasmendi <leo.isasmendi@gmail.com>
2020-02-25 13:41:58 -0300

    added Helper

---
+++ ImdbExample/app/src/main/java/com/example/imdbexample/Services/Helper.kt
@@ -0,0 +1,7 @@
+package com.example.imdbexample.Services
+
+object Helper {
+    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500/"
+    const val BASE_URL = "https://api.themoviedb.org/"
+    const val API_KEY = ""
+}
