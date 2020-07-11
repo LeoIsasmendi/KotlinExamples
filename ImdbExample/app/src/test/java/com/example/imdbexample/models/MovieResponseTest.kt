a6dcbe5baf934c81cde7d6c5182ae76ce5c6ca98
Leonardo Isasmendi <leo.isasmendi@gmail.com>
2020-07-11 19:37:18 -0300

    added MovieResponse test

---
+++ ImdbExample/app/src/test/java/com/example/imdbexample/models/MovieResponseTest.kt
@@ -0,0 +1,44 @@
+package com.example.imdbexample.models
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class MovieResponseTest {
+
+    private var page = 1
+    private var results = emptyList<Movie>()
+    private var totalResults = 1
+    private var totalPages = 1
+
+    private lateinit var model: MovieResponse
+
+    @Before
+    fun setUp() {
+        model = MovieResponse(page, results, totalResults, totalPages)
+    }
+
+    @Test
+    fun getPage() {
+        Assert.assertNotNull(model.page)
+        Assert.assertEquals(page, model.page)
+    }
+
+    @Test
+    fun getResults() {
+        Assert.assertNotNull(model.results)
+        Assert.assertEquals(results, model.results)
+    }
+
+    @Test
+    fun getTotal_results() {
+        Assert.assertNotNull(model.total_results)
+        Assert.assertEquals(totalResults, model.total_results)
+    }
+
+    @Test
+    fun getTotal_pages() {
+        Assert.assertNotNull(model.total_pages)
+        Assert.assertEquals(totalPages, model.total_pages)
+    }
+}
